b59f6837751642973ec21f02cb60e4d36d978f5d
ptdat4823 <phamtiendat4823@gmail.com>
2023-03-06 19:34:25 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          Hello this is new app
         </p>
         <a
           className="App-link"
